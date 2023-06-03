fef43283992cd25abd9336414800cb7fab571295
NishantGupt786 <84934511+NishantGupt786@users.noreply.github.com>
2023-06-04 02:05:14 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(emj) {
+  return <Card emoji={emj.emoji} name={emj.name} meaning={emj.meaning} />;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
